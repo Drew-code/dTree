66fc44308b58c02a384c473c84a10e99e9132572
Drew-code <52090139+Drew-code@users.noreply.github.com>
2020-01-06 14:25:27 -0500

    Update Decision_Tree_Model.py

--- Decision_Tree_Model.py
+++ Decision_Tree_Model.py
@@ -6,16 +6,16 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
 
-data = pd.read_csv('Sample_data')
-data = test_data.drop('unwanted variable',axis=1)
-print(data.info())
-print(data.head())
+stock = pd.read_csv(r"D:\Datasets\Amazon_Stock.csv")
+stock = stock.drop('time_stamp',axis=1)
+print(stock.info())
+print(stock.head())
 
 
 
 
-X = data.drop(['y_variable'],axis=1)
-y = data['y_variable']
+X = stock.drop(['Change'],axis=1)
+y = stock['Change']
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -26,7 +26,10 @@ dtree.fit(X_train,y_train)
 
 dtree_predict = dtree.predict(X_test)
 
+dtree_predict = dtree.predict(X_test)
+print("Decision Tree Classification report")
 print(classification_report(y_test,dtree_predict))
+print("Decision Tree Confusion Matrix")
 print(confusion_matrix(y_test,dtree_predict))
 
 
